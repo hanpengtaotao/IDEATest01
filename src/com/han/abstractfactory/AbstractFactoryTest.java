878e8d764f22e8b7c55068d2a0f6944a0547f9aa
Administrator <Administrator@PC-20170516KCVK>
2020-01-13 14:58:13 +0800

    11

--- src/com/han/abstractfactory/AbstractFactoryTest.java
+++ src/com/han/abstractfactory/AbstractFactoryTest.java
@@ -1,9 +1,14 @@
 package com.han.abstractfactory;
 
+import java.util.Scanner;
+
 public class AbstractFactoryTest {
     public static void main(String[] args) {
         IWorkFactory workFactory = new JavaWorkFactory();
         workFactory.createCode().WriteCode();
         workFactory.createHelpDoc().writeHelpDoc();
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(scanner.next());
     }
 }
